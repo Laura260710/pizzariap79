7af07073d288212ba5dcac63e28d46753235f1ef
Laura260710 <110499260+Laura260710@users.noreply.github.com>
2022-10-18 19:27:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,40 +1,31 @@
-var nomeDasPessoas = [];
-function submeter(){
+menuListArray = ["Pizza Vegetariana","Pizza de calabresa", "Pizza de carne", "Pizza de milho", "Pizza de frango"
+                    ];
 
-    var nomeConvidado = document.getElementById("nome1").value;
-    nomeDasPessoas.push(nomeConvidado);
-    console.log(nomeConvidado);
-    console.log(nomeDasPessoas);
-
-    var comprimentoNome = nomeDasPessoas.length;
-    console.log(comprimentoNome);
-    document.getElementById("mostrarNome").innerHTML=nomeDasPessoas.toString();
+function getMenu(){
+var htmldata=" ";
+menuListArray.sort();
+for(var i=0;i<menuListArray.length; i++){
+    htmldata=htmldata+imgtags +menuListArray [i] + '<br>';
 }
-
-function ordemAlfabetica() {
-nomeDasPessoas.sort(); 
-var i = nomeDasPessoas.join("<br>");
-console.log(nomeDasPessoas);
-document.getElementById("organizado").innerHTML = i.toString();
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
 
 }
 
-function mostrar() {
-var i = nomeDasPessoas.join("<br>");
-console.log(nomeDasPessoas);
-document.getElementById("p1").innerHTML=i.toString();
-document.getElementById("alfabeto").style.display="block";
+function addItem(){
+var htmldata;
+var imgtags= '<img id="img1" src="https://i2.wp.com/www.multarte.com.br/wp-content/uploads/2019/03/pizza-png-fundo-transparente19.png?fit=696%2C432&ssl=1"/>'
+var item=document.getElementById("addItem").value;
+menuListArray.sort();
+htmldata=" "
+for(var i=0;i<menuListArray.length; i++){
+    htmldata=htmldata+imgtags +menuListArray [i] + '<br>';
+}
+document.getElementById("displayAddedMenu").innerHTML = htmldata;
+
 }
 
-function pesquisar(){
-var s =document.getElementById("s1").value;
-var localizar = 0;
-var j; 
-for(j=0; j<nomeDasPessoas.length; j++){
-    if(s==nomeDasPessoas[j]){
-        localizar=localizar+1;
-    }
- }
- document.getElementById("p2").innerHTML="nome encontrado "+ localizar+" vez(es)";
-console.log("nome encontrado " + localizar + " vez(es)") 
+function addTop(){
+var item=document.getElementById("addItem").value;
+menuListArray.push(item);
+addItem();
 }
